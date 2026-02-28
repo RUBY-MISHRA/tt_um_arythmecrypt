4f3c61a4220ab49caba533cc819a7de82c630fba
jkdasfet <jkdasfet@kiit.ac.in>
2026-02-28 09:50:53 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -12,7 +12,7 @@ module tt_um_arythcrypto (
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    //input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
@@ -23,7 +23,7 @@ module tt_um_arythcrypto (
   assign uio_oe  = 0;//all io act as input pin, hence it is 0
 
   // List all unused inputs to prevent warnings
-  //wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, clk, rst_n, 1'b0};
     // Expanding 6 bit to 8 bit input after zero padding
     wire [7:0] A = {1'b0,1'b0, ui_in[5:0]};
     wire [7:0] B = {1'b0,1'b0, uio_in[5:0]};
