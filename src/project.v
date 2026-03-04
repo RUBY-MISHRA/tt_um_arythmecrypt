1640393ccac28a076cef6f2817d99930bfd2e8a4
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-05 00:57:19 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,7 +19,7 @@ module tt_um_arythcrypto (
 
   // All output pins must be assigned. If not used, assign to 0.
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
- // assign uio_out = 0; //uio_out not used , hence its 0
+  assign uio_out = 0; //uio_out not used , hence its 0
   assign uio_oe  = 0;//all io act as input pin, hence it is 0
 
   // List all unused inputs to prevent warnings
