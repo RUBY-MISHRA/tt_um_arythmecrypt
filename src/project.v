fd00f182d569ab3394d09ccc3c50dbad1d1ca81b
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2026-03-05 00:02:18 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -19,11 +19,11 @@ module tt_um_arythcrypto (
 
   // All output pins must be assigned. If not used, assign to 0.
  // assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0; //uio_out not used , hence its 0
+ // assign uio_out = 0; //uio_out not used , hence its 0
   assign uio_oe  = 0;//all io act as input pin, hence it is 0
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena,1'b0};
     // Expanding 6 bit to 8 bit input after zero padding
     wire [7:0] A = {1'b0,1'b0, ui_in[5:0]};
     wire [7:0] B = {1'b0,1'b0, uio_in[5:0]};
